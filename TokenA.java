1ba2735f45ef8630059ceb02f628f7d9de33bb1e
MuSobanAkbar <mu.sobanakbar@gmail.com>
2025-04-01 02:08:31 +0400

    Add files via upload
    31st March 2025 Update (07:08PM ADT) - the idea of storing arrys in arrays has been removed. It will now focus on creating a singular array.

--- TokenA.java
+++ TokenA.java
@@ -1,13 +1,14 @@
 //This class is made to test how tokenizing works.
+// 31st March 2025 Update - the idea of storing arrys in arrays has been removed. It will now focus on creating a singular array.
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class TokenA
 {
 	public static void main(String[]args)
 	{
 		Scanner scanner = new Scanner(System.in);
 		String delimiter = ",";
-		System.out.println("How many tokens do you want to read in (i.e. how many times do you want to enter): "); //assuming the user enters an integer.
+		System.out.println("How much data are you going to add (this is how many tokens you want to read in): "); //assuming the user enters an integer.
 		int times = scanner.nextInt();
 		scanner.nextLine();
 		System.out.println("What type of data type are you going to enter in?: ");//assuming the user enters the right data type.
@@ -22,42 +23,31 @@ public class TokenA
 
 		if(sentence.equals("string"))
 		{
-			String[][] arrayString = new String[times][];
-			System.out.println("How many tokens do you plan on entering?: ?"); //by 'plan' it is asking directly.
-			int counter = scanner.nextInt();
-			scanner.nextLine();
-			String[] embeddedArrayString = new String[counter];
-			for(int i =0; i<arrayString.length; i++)
-			{
+			String[] arrayString = new String[times];
+			System.out.println("Enter your string:");
+			String sentenceS = scanner.nextLine();
+			Scanner scannerString = new Scanner(sentenceS).useDelimiter(delimiter);
+			boolean hasNext = scannerString.hasNext();
 
-				scanner.nextLine();
-				System.out.println("Enter your string:");
-				String sentenceS = scanner.nextLine();
-				Scanner scannerString = new Scanner(sentenceS).useDelimiter(delimiter);
-				boolean hasNext = scannerString.hasNext();
-				for(int j=0; j<embeddedArrayString.length; j++)
-				{
-					if(scannerString.hasNext())
-					{
-						embeddedArrayString[j] = scannerString.next();
-					}
-					
-					
-				}
-				arrayString[i] = embeddedArrayString;
+			for(int i =0; i<arrayString.length && hasNext; i++)
+			{
+				arrayString[i] = scannerString.next();
+		
+			}
+			
 	
 				
-			}
 			System.out.println("Working.");
 			System.out.println("Below is a representation of your array");
 			for(int i=0;i<arrayString.length;i++)
 			{
-				System.out.println(Arrays.toString(arrayString[i]));
+				System.out.println(arrayString[i]);
 			}
-
 		}
 
-
+	}
 }
 
-}
+
+
+
